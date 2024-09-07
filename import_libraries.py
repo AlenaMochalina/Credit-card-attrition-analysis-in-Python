bde514871dbbabb51cd9af1586b7ba805e2be041
Rad1o0 <radektvrdy@gmail.com>
2024-09-07 13:36:39 +0200

    push import_libraries.py

---
+++ import_libraries.py
@@ -0,0 +1,30 @@
+def import_libraries():
+    try:
+        # Data manipulation libraries
+        import pandas as pd
+        import numpy as np
+
+        # Plotting libraries
+        import matplotlib.pyplot as plt
+        import seaborn as sns
+        from matplotlib.ticker import FixedLocator
+
+        # Preprocessing libraries
+        from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
+        from sklearn.model_selection import train_test_split
+
+        # Modeling libraries
+        from sklearn.neighbors import KNeighborsClassifier
+        from sklearn.model_selection import GridSearchCV
+        from sklearn.cluster import KMeans
+        import pickle
+
+        # Evaluation libraries
+        from sklearn.metrics import classification_report, confusion_matrix
+
+        print("All specified libraries have been imported successfully.")
+    except ImportError as e:
+        print(f"Error importing libraries: {e}")
+
+# Call the function to import the libraries
+import_libraries()
